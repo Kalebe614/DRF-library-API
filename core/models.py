522e218f62311cdaa03c8c5303671b48e37d1df0
kalebe <kalebe613@gmail.com>
2023-07-13 14:38:48 +0100

    Create publisher

--- core/models.py
+++ core/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class PublisherModel(BaseModel):
+    name = models.CharField('Name', max_length=50)
+    description = models.TextField('Description')
+
+    def __str__(self):
+        return self.name
