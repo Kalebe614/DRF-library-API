38311ac79fff0fc9c2144f5e35e0caa1a219f053
kalebe <kalebe613@gmail.com>
2023-07-13 18:10:29 +0100

    Create a route to book by authorCreate a route to show all books by the author

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, mixins
 from .models import PublisherModel, AuthorModel, BookModel
 from .serializers import PublisherSerializer, AuthorSerializer, BookSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class PublisherViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
                        mixins.UpdateModelMixin, mixins.DestroyModelMixin,
@@ -15,6 +17,14 @@ class AuthorViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
     
     queryset = AuthorModel.objects.all()
     serializer_class = AuthorSerializer
+
+    @action(detail=True, methods=['get'])
+    def books(self,request, pk=None):
+        author = self.get_object()
+        books = BookModel.objects.filter(author=author)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
 
 class BookViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
                   mixins.UpdateModelMixin, mixins.DestroyModelMixin,
