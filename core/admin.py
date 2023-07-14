1d67bec13a3773d79236b0cef019133224b2f82f
kalebe <kalebe613@gmail.com>
2023-07-14 19:25:11 +0100

    Update BookModel and BookViewSet

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BookModel, AuthorModel
+@admin.register(BookModel)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author','publisher','language','pages','created', 'updated' ]
 
-# Register your models here.
+@admin.register(AuthorModel)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name','about','created', 'updated']
